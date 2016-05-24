6d54126e41e7c3049b4039a2154491a96e6007c5
thoba <lose.thoba@gmail.com>
2016-05-24 11:22:33 +0200

    Increased sleep.

--- monitor_traffic.sh
+++ monitor_traffic.sh
@@ -3,7 +3,7 @@
 # https://github.com/bgruening/docker-ipython-notebook/blob/master/monitor_traffic.sh
 echo "--- Monitoring Traffic ---"
 while true; do
-    sleep 60
+    sleep 5m
     if [ `netstat -t | grep -v CLOSE_WAIT | grep ':7474' | wc -l` -lt 3 ]
     then
         pkill -f 'java'
